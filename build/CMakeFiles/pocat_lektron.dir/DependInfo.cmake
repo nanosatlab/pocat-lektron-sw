ed753957daaa7d866dac25e5076f527108b31bed
Arnau <arnau.dolz@estudiantat.upc.edu>
2024-05-03 19:58:36 +0200

    first push

---
+++ build/CMakeFiles/pocat_lektron.dir/DependInfo.cmake
@@ -0,0 +1,81 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/arnau/development/pocat-lektron-sw/startup_stm32l476rgtx.s" "/home/arnau/development/pocat-lektron-sw/build/CMakeFiles/pocat_lektron.dir/startup_stm32l476rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32L476RGxx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32L476xx_HAL_Driver/Inc"
+  "../Drivers/STM32L476xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32L476xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../"
+  "../home/arnau/development/pocat-lektron-sw/src"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rtc.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rtc.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rtc.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rtc_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rtc_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rtc_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/list.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj.d"
+  "/home/arnau/development/pocat-lektron-sw/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj" "gcc" "CMakeFiles/pocat_lektron.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
