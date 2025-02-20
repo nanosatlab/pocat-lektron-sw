3a0e3f5c72c77e2475b338d8125a0ef73bb9eaca
Oscar <oscarpavon2003@gmail.com>
2025-02-20 18:27:30 +0100

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -991,7 +991,7 @@ static void PAYLOAD_Task(void *params)
 	uint32_t RX_PAYLOAD_NOTIS,dT;
 	EventBits_t EventBits;
 
-	uint8_t resolution = 0x00;      //0x11 o 0x00
+	uint8_t resolution = 0x11;      //0x11 o 0x00
 	uint8_t compressibility = 0xFF; // 0x00 --- 0xFF
 	uint8_t info[50];
 
