edf9050195d1689036518a1113bdfcf69c1fe43c
arnau_dolz <arnau.dolz@i2cat.net>
2025-03-16 13:24:04 +0100

    adding licence, and updating gitignore

---
+++ build/CMakeFiles/pocat_lektron_sw.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for pocat_lektron_sw.elf.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/pocat_lektron_sw.elf.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for pocat_lektron_sw.elf.
