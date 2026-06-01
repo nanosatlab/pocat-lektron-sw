382fb347c3436703a737dfdbf506e85c53218b56
Guillermo O'Tuama Pascual <guillermo.o.tuama@estudiantat.upc.edu>
2026-06-01 15:13:59 +0200

    Update FreeRTOS kernel 10.3.1 to 11.2.0 which is the latest release ST provides for the stm32l4. Everything compiles correctly.

--- include/core/FreeRTOSConfig.h
+++ include/core/FreeRTOSConfig.h
@@ -65,6 +65,7 @@
 #define configUSE_PREEMPTION                     1
 #define configSUPPORT_STATIC_ALLOCATION          1
 #define configSUPPORT_DYNAMIC_ALLOCATION         1
+#define configKERNEL_PROVIDED_STATIC_MEMORY      1
 #define configUSE_IDLE_HOOK                      0
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
